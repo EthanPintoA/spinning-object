94d5bc24b0cf026df2a115df2f83777c2396cb34
Ethan Pinto <ethanpintoa@gmail.com>
2023-04-18 17:52:22 -0400

    Add line drawing

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,28 @@
+use glam::{IVec2, Vec2, Vec2Swizzles};
+
+const BUF_WIDTH: usize = 26;
+const BUF_HEIGHT: usize = 26;
+
 struct DisplayBuffer<const N: usize, const M: usize>(pub [[u8; M]; N]);
 
 fn main() {
-	let mut display_buf = DisplayBuffer([[0; 12]; 12]);
+	let mut display_buf = DisplayBuffer([[0; BUF_WIDTH]; BUF_HEIGHT]);
 
-	display_buf.0[0][0] = u8::MAX;
-	display_buf.0[0][10] = u8::MAX;
-	display_buf.0[10][0] = u8::MAX;
-	display_buf.0[10][10] = u8::MAX;
+	let triangle = [
+		Vec2::new(1.0, 1.0),
+		Vec2::new(22.0, 4.0),
+		Vec2::new(4.0, 22.0),
+	];
+
+	for pixel in get_triangle_pixels(&triangle) {
+		if !(0..BUF_HEIGHT).contains(&(pixel.y as usize)) {
+			return;
+		}
+		if !(0..BUF_WIDTH).contains(&(pixel.x as usize)) {
+			return;
+		}
+		display_buf.0[pixel.y as usize][pixel.x as usize] = u8::MAX;
+	}
 
 	let mut print_buf = String::new();
 	for row in display_buf.0 {
@@ -19,4 +35,76 @@ fn main() {
 	}
 
 	println!("{}", print_buf);
+}
+
+/// Returns vector positions dependent positions
+///
+/// `i` is for independent `d` is for dependent.
+///
+/// Example:
+/// ```
+/// let (x0, y0) = (0, 0);
+/// let (x1, y1) = (1, 2);
+///
+/// let positions = if (y1 - y0).abs() < (x1 - x0).abs() {
+///     // Slope is less than 1.0
+///     interpolate(x0, y0, x1, y1);
+/// } else {
+///     interpolate(x0, y0, x1, y1);
+/// }
+/// ```
+///
+/// Source: https://en.wikipedia.org/wiki/Bresenham%27s_line_algorithm
+fn interpolate_line(i0: i32, d0: i32, i1: i32, d1: i32) -> Vec<(i32, i32)> {
+	if i0 == i1 {
+		return vec![(i0, d0)];
+	}
+
+	// Make sure i1 > i0
+	let (i0, d0, i1, d1) = if i1 < i0 {
+		(i1, d1, i0, d0)
+	} else {
+		(i0, d0, i1, d1)
+	};
+
+	let slope = (d1 - d0) as f32 / (i1 - i0) as f32;
+
+	let get_d = |i| slope * i + d0 as f32;
+
+	(i0..=i1)
+		.enumerate()
+		.map(|(idx, i)| (i, get_d(idx as f32) as i32))
+		.collect()
+}
+
+/// Returns vector of positions representing the line on a raster.
+fn get_bresenhams_line(p0: IVec2, p1: IVec2) -> Vec<IVec2> {
+	if (p1.y - p0.y).abs() <= (p1.x - p0.x).abs() {
+		// If slope is less than 1.0, independent is x
+		interpolate_line(p0.x, p0.y, p1.x, p1.y)
+			.into_iter()
+			.map(IVec2::from)
+			.collect()
+	} else {
+		// If slope is greater than 1.0, independent is y
+		interpolate_line(p0.y, p0.x, p1.y, p1.x)
+			.into_iter()
+			.map(IVec2::from)
+			.map(Vec2Swizzles::yx)
+			.collect()
+	}
+}
+
+fn get_triangle_pixels(triangle: &[Vec2; 3]) -> Vec<IVec2> {
+	let edges = [
+		(triangle[0], triangle[1]),
+		(triangle[0], triangle[2]),
+		(triangle[1], triangle[2]),
+	];
+
+	edges
+		.into_iter()
+		.map(|(v0, v1)| (v0.round().as_ivec2(), v1.round().as_ivec2()))
+		.flat_map(|(v0, v1)| get_bresenhams_line(v0, v1))
+		.collect()
 }
