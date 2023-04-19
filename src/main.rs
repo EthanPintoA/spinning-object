457d8c71e1b19dc060a214e634207c32bd8551a3
Ethan Pinto <ethanpintoa@gmail.com>
2023-04-18 20:42:22 -0400

    Render cube mesh

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,7 @@ mod mesh;
 
 use draw::DisplayBuffer;
 
-use crate::mesh::Triangle;
+use crate::mesh::get_cube_mesh;
 
 const BUF_WIDTH: usize = 26;
 const BUF_HEIGHT: usize = 26;
@@ -12,18 +12,7 @@ const BUF_HEIGHT: usize = 26;
 fn main() {
 	let mut display_buf = DisplayBuffer([[0; BUF_WIDTH]; BUF_HEIGHT]);
 
-	let mesh = [
-		Triangle::new([
-			[1.0, 1.0, 0.0].into(),
-			[22.0, 4.0, 0.0].into(),
-			[4.0, 22.0, 0.0].into(),
-		]),
-		Triangle::new([
-			[22.0, 4.0, 0.0].into(),
-			[10.0, 22.0, 0.0].into(),
-			[24.0, 20.0, 0.0].into(),
-		]),
-	];
+	let mesh = get_cube_mesh([12.0, 12.0, 0.0].into(), 12.0);
 
 	display_buf.draw_mesh(&mesh);
 	println!("{}", display_buf);
