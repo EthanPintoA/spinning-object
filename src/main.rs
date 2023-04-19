90f01458de5b665aa66618bbc61e7d48b1f90455
Ethan Pinto <ethanpintoa@gmail.com>
2023-04-18 23:56:18 -0400

    Increase cube size

--- src/main.rs
+++ src/main.rs
@@ -9,16 +9,16 @@ use glam::{EulerRot, Quat, Vec3};
 
 use crate::mesh::{get_cube_mesh, rotate_mesh};
 
-const BUF_WIDTH: usize = 26;
-const BUF_HEIGHT: usize = 26;
+const BUF_WIDTH: usize = 50;
+const BUF_HEIGHT: usize = 50;
 
 const LIGHT_DIRECTION: Vec3 = Vec3::new(0.0, 0.0, 1.0);
 
 fn main() {
 	let mut display_buf = DisplayBuffer([[0; BUF_WIDTH]; BUF_HEIGHT]);
 
-	let mesh_center = [12.0, 12.0, 0.0].into();
-	let mut mesh = get_cube_mesh(mesh_center, 12.0);
+	let mesh_center = Vec3::new(BUF_WIDTH as f32 / 2.0, BUF_HEIGHT as f32 / 2.0, 0.0);
+	let mut mesh = get_cube_mesh(mesh_center, BUF_HEIGHT as f32 / 2.0);
 	let quat = Quat::from_euler(EulerRot::XYZ, PI / 16.0, PI / 8.0, PI / 8.0);
 
 	rotate_mesh(&mut mesh, quat, mesh_center);
